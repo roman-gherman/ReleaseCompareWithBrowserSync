c3ee8c04dddff43108af3448a56844d96e62c148
Roman Gherman <roman.gherman@infigosoftware.com>
2020-05-24 22:46:46 +0300

    First version - browser sync with proxy and config file.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# OldVsNewReleaseCompare
-Simple tool that uses browser sync to compare old versus new Release version of your web application
+# ReleaseCompareWithBrowserSync
+Simple tool that uses browser sync to compare old versus new Release version of your web application.
+
+The idea is simple, I create a proxy: https://localhost:8000
+When you open https://localhost:8000 in a browser - there is no cookie saved for it, so we open the first site from config.json.
+When you open the same https://localhost:8000 in another browser - and there is no cookie saved for it then we will open the next site from config.json and so on.
+
+If we reach the last site, then it will go from the beginning.
+
+At this time, this is the only tool that can work with sites that are on https. 
+
+Use Config.json to configure the sites you want to open through proxy.
+{
+  "platforms": ["https://release-1.domain.com", "https://release-2.domain.com"]
+}
+
+Dependencies:
+Node.JS
+
+How to Run:
+1. change config.json to match your sites. Note: both sites should be pretty much the same and have similar DB.
+2. Run install.ps1 with Powershell (ONLY FIRST TIME), this will do all the npm installations
+3. Run run.ps1 to start the proxy and browser sync. Then open https://localhost:8000 in different browsers, check the Cookeis - to see the destination site.
+
+Contributions:
+Please feel free to contribute and make this tool more powerful.
+
+Ideas for the future:
+1. Allow choosing in browser the URL from config that you want to open
+2. Allow to reset the site in same browser to a different site, on the fly.
---
+++ index.js
@@ -0,0 +1,130 @@
+const roundround = require('roundround');
+const https = require('https');
+const httpProxy = require('http-proxy');
+const fs = require('fs');
+const path = require('path');
+
+const httpsOpts = {
+    key: fs.readFileSync(path.join(__dirname, 'agent2-key.pem'), 'utf8'),
+    cert: fs.readFileSync(path.join(__dirname, 'agent2-cert.pem'), 'utf8')
+};
+
+var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+var nextDestinationRoundRobin = roundround(config.platforms);
+
+var proxy = httpProxy.createProxyServer({
+    secure: false, // we ignore SSL certificates check
+    autoRewrite: true,
+    ssl: httpsOpts,
+    protocolRewrite: "https",
+    changeOrigin: true,
+    selfHandleResponse : true
+});
+
+function getDestinatonFromCookie(req)
+{
+    var cookies = {};
+    req.headers.cookie && req.headers.cookie.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        cookies[ parts[ 0 ].trim() ] = ( parts[ 1 ] || '' ).trim();
+    });
+
+    return cookies.proxyDestination;
+}
+
+function getRegexFromUrl(url)
+{
+    return new RegExp(getHostFromUrl(url),"g");
+}
+
+function getHostFromUrl(url)
+{
+    var strToSearch = url.replace('https://', '');
+    if(strToSearch.endsWith('/'))
+    {
+        strToSearch = strToSearch.substring(0, strToSearch.length - 1);
+    }
+    return strToSearch;
+}
+
+https.createServer(httpsOpts, function (req, res) {
+    var destination = getDestinatonFromCookie(req);
+    if(!destination)
+    {
+        destination = nextDestinationRoundRobin();//"https://c2016.pp-islcatfish.com"
+        console.log(destination);
+    }
+
+    res.setHeader("proxy-destination", destination);
+    req.headers['proxy-destination'] = destination;
+
+    proxy.web(req, res, {
+        target: destination
+    });
+}).listen(8000);
+
+console.log("Application has started: https://localhost:8000");
+
+// Listen for the `error` event on `proxy`.
+proxy.on('error', function (err, req, res) {
+    console.log(err)
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+    res.end("Error");
+});
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    var proxyDestination = req.headers['proxy-destination'];
+    var host = getHostFromUrl(proxyDestination);
+    // save the host in header so that target site thinks request is done from the correct domain
+    proxyReq.setHeader('Host', host);
+});
+
+// We need to modify the response to save a cookie - same browser - will always stay on the same RELEASE version
+proxy.on('proxyRes', function(proxyRes, req, res) {
+    const bodyChunks = [];
+    proxyRes.on('data', (chunk) => {
+        bodyChunks.push(chunk);
+    });
+    proxyRes.on('end', () => {
+        const body = Buffer.concat(bodyChunks);
+
+        // forwarding source status
+        res.statusCode = proxyRes.statusCode;
+        delete proxyRes.headers['content-length'];
+
+        var cookiesWereSet = false;
+        var proxyDestination = req.headers['proxy-destination'];
+
+        // forwarding source headers
+        Object.keys(proxyRes.headers).forEach((key) => {
+            if(!cookiesWereSet && key.toLowerCase() == "set-cookie" && proxyDestination)
+            {
+                var val = proxyRes.headers[key];
+                val.push("proxyDestination=" + proxyDestination + ';path=/')
+                res.setHeader(key, val);
+                cookiesWereSet = true;
+            }
+            else {
+                res.setHeader(key, proxyRes.headers[key]);
+            }
+        });
+
+        if(proxyDestination && !cookiesWereSet)
+        {
+            res.setHeader('set-cookie', "proxyDestination=" + proxyDestination + ';path=/');
+        }
+
+        // modifying html content
+        if (proxyRes.headers['content-type'] && proxyRes.headers['content-type'].includes('text/html')) {
+            let html = body.toString();
+            html = html.replace(getRegexFromUrl(proxyDestination), 'localhost:8000');
+            res.end(new Buffer.from(html));
+        } else {
+            res.end(body);
+        }
+
+        res.end();
+    });
+});
